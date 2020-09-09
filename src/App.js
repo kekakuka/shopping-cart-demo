2d7ed5c920339ec7c9672bb9406226d790d70ff2
kekakuka <39260972@qq.com>
2020-09-09 17:34:38 +1200

    add drawer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Product from './pages/Product';
 import './App.css';
-
+import Drawer from './components/Drawer';
 function App() {
   return (
     <div className='App'>
+      <Drawer />
       <Product />
     </div>
   );
---
+++ src/components/Drawer/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const useStyles = makeStyles({
+  list: {
+    width: 450,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+export default function TemporaryDrawer() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={classes.list}
+      role='presentation'
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['p1', 'p2', 'p3'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <React.Fragment key={'left'}>
+      <Button onClick={toggleDrawer('left', true)}>
+        <ShoppingCartIcon />
+      </Button>
+      <Drawer anchor={'left'} open={state['left']} onClose={toggleDrawer('left', false)}>
+        {list('left')}
+      </Drawer>
+    </React.Fragment>
+  );
+}
